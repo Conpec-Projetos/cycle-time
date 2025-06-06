5bb828ffd91832164d322817b5ef93ac96c45750
yellowisk <oheitoralmeida2013@gmail.com>
2025-06-06 01:12:02 -0300

    update cycle in OKRs dashboard

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import os
 import gspread
 from notion_client import Client
 from typing import List, Tuple
+import pandas as pd
+# from dotenv import load_dotenv
+# load_dotenv()
 
 NOTION_TOKEN = os.environ["NOTION_TOKEN"]
 CSN_DB_ID = os.environ["CSN_DB_ID"]
-CHAMEX_DB_ID = os.environ["CHAMEX_DB_ID"]
 ADUNICAMP_DB_ID = os.environ["ADUNICAMP_DB_ID"]
-
-notion_dbs = [CSN_DB_ID, CHAMEX_DB_ID, ADUNICAMP_DB_ID]
 
 notion = Client(auth=NOTION_TOKEN)
 gc = gspread.service_account(filename="credentials.json")
@@ -25,7 +25,6 @@ STATUS_MAP = {
 
 SHEETS_MAP = {
     "CSN | Burndown Chart": CSN_DB_ID,
-    "CHAMEX | Burndown Chart": CHAMEX_DB_ID,
     "ADunicamp | Burndown Chart": ADUNICAMP_DB_ID
 }
 
@@ -84,6 +83,59 @@ def update_all_sheets():
             update_sheet(tasks, sheet_name)
         else:
             print(f"Nenhuma tarefa encontrada para {sheet_name}")
+            
+def calculate_cycle_times(sheet_name: str) -> pd.DataFrame:
+    sheet = gc.open(sheet_name).sheet1
+    values = sheet.get_all_values()
+
+    columns = ['Tarefa', 'Status Secundário', 'Status Principal', 'Data Movimentação']
+    df = pd.DataFrame(values, columns=columns)
+
+    df['Data Movimentação'] = pd.to_datetime(df['Data Movimentação'], errors='coerce')
+
+    grouped = df.groupby(['Tarefa', 'Status Principal'])
+
+    started = grouped['Data Movimentação'].min().unstack()
+
+    started['Cycle Time (dias)'] = (
+        started.get('Completo') - started.get('Em progresso')
+    ).dt.days
+
+    result_df = started.reset_index()[['Tarefa', 'Cycle Time (dias)']]
+
+    return result_df
+
+def calculate_global_cycle_time(sheet_names: List[str]) -> float:
+    all_cycle_times = []
+
+    for sheet_name in sheet_names:
+        cycle_df = calculate_cycle_times(sheet_name)
+
+        project_cycle_times = cycle_df['Cycle Time (dias)'].dropna().tolist()
+        if not project_cycle_times:
+            print(f"Nenhum cycle time encontrado para o projeto {sheet_name}.")
+            continue
+        
+        project_sheet = gc.open(sheet_name).worksheet("Cycle Time")
+
+        last_row = len(project_sheet.get_all_values()) + 1
+        project_sheet.update_cell(last_row, 1, round(sum(project_cycle_times) / len(project_cycle_times), 2))
+        project_sheet.update_cell(last_row, 2, pd.Timestamp.now().strftime('%Y-%m-%d %H:%M:%S')) 
+        
+        all_cycle_times.extend(project_cycle_times)
+
+    if all_cycle_times:
+        global_average = sum(all_cycle_times) / len(all_cycle_times)
+        return round(global_average, 2)
+    else:
+        print("Nenhum cycle time encontrado para os projetos.")
+        return 0.0
+    
+def update_cycle_time_in_okrs_sheet():
+    okrs_sheet = gc.open("0. [Conpec][2025] Painel Estratégico de 2025").worksheet("Dashboard")
+    cycle_time = calculate_global_cycle_time(list(SHEETS_MAP.keys()))
+    okrs_sheet.update([[cycle_time]], range_name="G21")
 
 if __name__ == "__main__":
     update_all_sheets()
+    update_cycle_time_in_okrs_sheet()
